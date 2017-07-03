126c4424d949303b3424f1f83a95046cd4ad30c9
Amin Aghaei <am.aghaei@gmail.com>
2017-07-02 17:54:53 -0700

    Add the relative energy and errors calculation

--- rom_svd_matlab.m
+++ rom_svd_matlab.m
@@ -13,15 +13,24 @@ fprintf('The original image takes %7.0f KB of memory\n', origMemory)
     % [U] = [3n2 x 3n2]
     % [S] = [3n2 x  n1]
     % [V] = [ n1 x  n1]
+
+% Calculate the total energy corresponding to the original image
+totEnerg = 0;
+for i = 1:min(size(S,1), size(S,2))
+    totEnerg = totEnerg + S(i,i)*S(i,i);
+end
     
 % For ploting the images
 figure1 = figure('units','normalized','outerposition',[0 0 1 1]);   set(gcf,'color','w')
 
 for kk = 1:4
     k = kk * ceil(nBasesMax / 4);
+    
+    romEnerg = 0; % Reduced order energy
     rImage = zeros(size(unrollImage));   % [3n2 x n1]
     for i=1:k
         rImage = rImage + S(i,i) * (U(:,i)*V(:,i)');
+        romEnerg = romEnerg + S(i,i)*S(i,i);
     end
     % So we have kept only (3n2 + n1) x nBases information
     
@@ -32,9 +41,11 @@ for kk = 1:4
     rImageRoll(:,:,3) = rImage(:,2*ss+1:3*ss);
 
     memoryNeeded = (size(S,2) + size(S,1))*k / 1024;
+    relEnerg = romEnerg/totEnerg;
     fprintf('--------------- k = %d ------------------\n', k)
     fprintf('The rom-svd image takes %d KB of memory\n', memoryNeeded)
-    fprintf('The rom-svd image takes %5.2f%% memory compared to the origanl image\n', memoryNeeded/origMemory)
+    fprintf('The rom-svd image takes %5.2f memory compared to the origanl image\n', memoryNeeded/origMemory)
+    fprintf('The relative energy is: %6.3f. So the error in the energy of the snapshot is %5.2f%%\n', relEnerg, (1-relEnerg)*100)
 
     % plot
     sp = subplot(2,2,kk);
